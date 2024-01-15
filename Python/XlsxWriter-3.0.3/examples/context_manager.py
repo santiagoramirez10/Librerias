c79bb408bfdbf9ec19fbce985253683ed6fbbdd7
santiagoramirez10 <santiagoramirezperez96@gmail.com>
2024-01-15 08:46:27 -0500

    Python libraries-Fritzing parts

---
+++ Python/XlsxWriter-3.0.3/examples/context_manager.py
@@ -0,0 +1,14 @@
+##############################################################################
+#
+# A simple example using the XlsxWriter Python module and the "with" context
+# manager. This doesn't require an explicit close().
+#
+# SPDX-License-Identifier: BSD-2-Clause
+# Copyright 2013-2022, John McNamara, jmcnamara@cpan.org
+#
+import xlsxwriter
+
+with xlsxwriter.Workbook('hello_world.xlsx') as workbook:
+    worksheet = workbook.add_worksheet()
+
+    worksheet.write('A1', 'Hello world')
---
+++ Python/XlsxWriter-3.0.3/examples/hello_world.py
@@ -0,0 +1,15 @@
+##############################################################################
+#
+# A hello world spreadsheet using the XlsxWriter Python module.
+#
+# SPDX-License-Identifier: BSD-2-Clause
+# Copyright 2013-2022, John McNamara, jmcnamara@cpan.org
+#
+import xlsxwriter
+
+workbook = xlsxwriter.Workbook('hello_world.xlsx')
+worksheet = workbook.add_worksheet()
+
+worksheet.write('A1', 'Hello world')
+
+workbook.close()
